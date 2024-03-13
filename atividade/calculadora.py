b2a0c77aad0a9043df35c993203a703f90daf266
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-03-13 08:36:29 -0300

    Add files via upload

---
+++ atividade/calculadora.py
@@ -0,0 +1,11 @@
+numero1=float(input("digite o primeiro numero"))
+numero2=float(input("digite o segundo numero"))
+
+soma=numero1 + numero2
+subtracao=numero1 - numero2
+multiplicaçao=numero1 * numero2
+divisao=numero1 / numero2
+print("a soma é", soma)
+print("a subtraçao é", subtracao)
+print("a multiplicaçao é", multiplicaçao)
+print("a divisao é", divisao)
