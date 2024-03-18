f5ae383ce2993b9ecdd8a085ff1f13cbd8db456b
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-03-18 12:01:11 -0300

    Add files via upload

---
+++ numerosaleatorios.py
@@ -0,0 +1,6 @@
+import random
+numero_minimo=int(input("digite o valor minimo"))
+numero_maximo=int(input("digite o valor maximo"))
+
+numero_aletorio=random.randint(numero_minimo, numero_maximo)
+print("o numero aleatorio é", numero_aletorio)
